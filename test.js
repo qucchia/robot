ce1520f519081aae8b6a419cbfc8950edbfc60ef
qucchia <yijods@gmail.com>
2021-09-05 17:00:04 +0200

    Connect to cloud controls

--- test.js
+++ test.js
@@ -1,10 +1,37 @@
 import Wheels from "./wheels.js";
-let wheels = new Wheels(10, 9, 8, 7);
+import WebSocket from "ws";
 
-async function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+let ws = new WebSocket("wss://clouddata.turbowarp.org");
+let wheels = new Wheels(7, 8, 10, 9);
+wheels.stop();
+
+ws.onopen = () => {
+  console.log("Performing handshake");
+  
+  ws.send(JSON.stringify({
+    method: "handshake",
+    project_id: process.env.PROJECT_ID,
+    user: "qucchia"
+  }));
 };
 
-wheels.forward();
-await delay(3000);
-wheels.stop();
+ws.onmessage = (event) => {
+  for (const message of event.data.split("\n")) {
+    const obj = JSON.parse(message);
+    if (obj.method === "set") {
+      if (obj.name === "☁ Move") {
+        if (obj.value === 0) {
+          wheels.stop();
+        } else  if (obj.value === 1) {
+          wheels.forward();
+        } else if (obj.value === 2) {
+          wheels.left();
+        } else if (obj.value === 3) {
+          wheels.right();
+        } else if (obj.value === 4) {
+          wheels.backward();
+        }
+      }
+    }
+  }
+}
